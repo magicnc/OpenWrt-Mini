ad780fadc428e94ec4d7a59f00ed5e91eba23e78
magicnc <463689898@qq.com>
2024-11-21 13:43:50 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -26,4 +26,4 @@ sed -i 's/"CPU 性能优化调节"/"CPU调节"/g' `grep "CPU 性能优化调节"
 # sed -i 's/"Argon 主题设置"/"Argon设置"/g' `grep "Argon 主题设置" -rl ./`
 
 # 移动 短信 到 “网络” 子菜单 /cgi-bin/luci/admin/modem/sms
-sed -i 's/services/network/g' feeds/luci/applications/luci-app-sms-tool/root/usr/share/luci/menu.d/luci-app-sms-tool
+# sed -i 's/services/network/g' feeds/luci/applications/luci-app-sms-tool/root/usr/share/luci/menu.d/luci-app-sms-tool
